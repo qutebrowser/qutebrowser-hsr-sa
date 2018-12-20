5c828dfe510ec3abb479f6576aacc077accec4fa
Florian Bruhin <me@the-compiler.org>
2018-12-20 17:27:56 +0100

    Calculate per topic

--- time/analyze.py
+++ time/analyze.py
@@ -1,6 +1,9 @@
 import datetime
 import csv
 import json
+import collections
+import enum
+import pprint
 
 
 def parse_time(date, time):
@@ -15,12 +18,62 @@ def get_semweek(start, end):
     return start_week - 37
 
 
+class Topic(enum.Enum):
+
+    meetings = 1
+    misc = 2
+    upstream = 3
+    docs = 4
+    contributions = 5
+    bugfixes = 6
+    implementation = 7
+
+
 def analyze(f):
     r = csv.DictReader(f)
 
     weekly_hours = [0.0] * 14
+    per_topic = collections.defaultdict(float)
+    per_desc = collections.defaultdict(float)
+
+    topic_map = {
+        'Sitzung': Topic.meetings,
+        'Sitzung (Nachbereitung)': Topic.meetings,
+        'Video call mit fiete': Topic.meetings,
+        'Videocall fiete': Topic.meetings,
+        'Sitzung (Vorbereitung)': Topic.meetings,
+        'Sitzung Sprachberatung': Topic.meetings,
+        'Sitzung (API review)': Topic.meetings,
+        'Sprachreview': Topic.meetings,
+
+        'Einführung': Topic.misc,
+        'Community': Topic.misc,
+        'Release': Topic.misc,
+        'Tracking': Topic.misc,
+        'Buch lesen': Topic.misc,
+        'Organisatorisches': Topic.misc,
+
+        'Upstream': Topic.upstream,
+        'pytest upgrade': Topic.upstream,
+        'Qt 5.12': Topic.upstream,
+
+        'Dokumentation': Topic.docs,
+        'Doku': Topic.docs,
+        'Dokumentation (Projektplan)': Topic.docs,
+        'Sphinx': Topic.docs,
+
+        'Implementation': Topic.implementation,
+        'API review implementation': Topic.implementation,
+        'Implementation (dynamic loading)': Topic.implementation,
+        'mypy': Topic.implementation,
+        'Bugfixes': Topic.implementation,
+        'Bugfixing': Topic.implementation,
+
+        'PRs': Topic.contributions,
+    }
 
     for entry in r:
+        print(entry)
         desc = entry['Description']
         if not desc:
             continue
@@ -37,18 +90,33 @@ def analyze(f):
         assert hours == float(entry['Duration (decimal)'])
 
         semweek = get_semweek(start_dt, end_dt)
+        topic = topic_map[desc].name
 
         weekly_hours[semweek - 1] += hours
+        per_topic[topic] += hours
+        per_desc[(topic, desc)] += hours
 
-    return weekly_hours
+    return weekly_hours, per_topic, per_desc
 
 
 def main():
     with open('time.csv', 'r', encoding='utf-8') as f:
-        weekly_hours = analyze(f)
+        weekly_hours, per_topic, per_desc = analyze(f)
+
+    print('\n===== weekly =====')
+    pprint.pprint(weekly_hours)
+
+    print('\n===== per topic =====')
+    pprint.pprint(dict(per_topic))
+
+    print('\n===== per desc =====')
+    pprint.pprint(dict(per_desc))
 
     with open('weekly.json', 'w') as f:
         json.dump(weekly_hours, f)
+
+    with open('per_topic.json', 'w') as f:
+        json.dump(per_topic, f)
 
 
 if __name__ == '__main__':
