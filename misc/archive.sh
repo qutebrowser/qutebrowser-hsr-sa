764f99f082267232bd3ec79d2777771801fc3792
Florian Bruhin <me@the-compiler.org>
2018-12-21 02:33:38 +0100

    Get ready for release

--- misc/archive.sh
+++ misc/archive.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -e
+
 abgabedir=~/proj/qutebrowser/sa-private/abgabe
 archivedir=$abgabedir/archive
 uploaddir=$abgabedir/upload
 sadir=~/proj/qutebrowser/sa
-privatedir=~/proj/qutebrowser/sa-private
+saprivate=~/proj/qutebrowser/sa-private
 
 if [[ -d "$abgabedir" ]]; then
     echo "Existing dir found, exiting." >&1
@@ -15,8 +17,8 @@ mkdir "$abgabedir"
 mkdir "$archivedir"
 
 mkdir "$archivedir/repos"
-git clone https://github.com/qutebrowser/qutebrowser "$archivedir/repos/qutebrowser"
-git clone https://github.com/qutebrowser/qutebrowser-hsr-sa "$archivedir/repos/qutebrowser-hsr-sa"
+git clone ~/proj/qutebrowser/git "$archivedir/repos/qutebrowser"
+git clone "$sadir" "$archivedir/repos/qutebrowser-hsr-sa"
 git clone https://github.com/qutebrowser/qutebrowser-hsr-sa.wiki.git "$archivedir/repos/notes"
 
 cat << EOF > "$archivedir/readme.txt"
@@ -30,19 +32,20 @@ Aufgabenstellung: repos/qutebrowser-hsr-sa/doc/img/SA_Bruhin_Aufgabenstellung_v2
 EOF
 
 cp "$sadir/doc/main.pdf" "$archivedir/sa-bruhin-qutebrowser.pdf"
-# cp "$sadir/poster/main.pdf" "$archivedir/sa-bruhin-qutebrowser-poster.pdf"
-cp "$sadir/misc/plain-abstract.txt" "$archivedir"
 
 mkdir "$archivedir/legal"
 cp "$saprivate/eigenstaendigkeit.pdf" "$archivedir/legal"
+cp "$saprivate/eigenstaendigkeit.odt" "$archivedir/legal"
 cp "$saprivate/urheberrechte.pdf" "$archivedir/legal"
+cp "$saprivate/urheberrechte.odt" "$archivedir/legal"
 cp "$saprivate/publikation.pdf" "$archivedir/legal"
+cp "$saprivate/publikation.odt" "$archivedir/legal"
 
 mkdir "$uploaddir"
 ln -s "$archivedir/sa-bruhin-qutebrowser.pdf" "$uploaddir/sa.pdf"
-ln -s "$archivedir/sa-bruhin-qutebrowser-poster.pdf" "$uploaddir/poster.pdf"
+ln -s "$archivedir/repos/qutebrowser-hsr-sa/poster/poster.pdf" "$uploaddir/poster.pdf"
 ln -s "$archivedir/legal/eigenstaendigkeit.pdf" "$uploaddir/eigenstaendigkeit.pdf"
 ln -s "$archivedir/legal/urheberrechte.pdf" "$uploaddir/urheberrechte.pdf"
 ln -s "$archivedir/legal/publikation.pdf" "$uploaddir/publikation.pdf"
-ln -s "$archivedir/plain-abstract.txt" "$uploaddir/plain-abstract.txt"
-zip -r "$archivedir"/* "$uploaddir/archive.zip"
+ln -s "$archivedir/repos/qutebrowser-hsr-sa/misc/plain-abstract.txt" "$uploaddir/plain-abstract.txt"
+zip -r "$uploaddir/archive.zip" "$archivedir"/* 
